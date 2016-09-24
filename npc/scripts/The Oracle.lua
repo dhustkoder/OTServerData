f8f2efbcf2ea3e7ca323003ccaebc7f4c5529fd4
dhust <rafaelmoura.dev@gmail.com>
2016-09-24 09:13:38 -0300

    step back

--- npc/scripts/The Oracle.lua
+++ npc/scripts/The Oracle.lua
@@ -1,127 +1,104 @@
-local LEVEL = 8
-
 local keywordHandler = KeywordHandler:new()
 local npcHandler = NpcHandler:new(keywordHandler)
 NpcSystem.parseParameters(npcHandler)
 
--- OTServ event handling functions
-function onCreatureAppear(cid)			npcHandler:onCreatureAppear(cid)			end
-function onCreatureDisappear(cid)		npcHandler:onCreatureDisappear(cid)			end
-function onCreatureSay(cid, type, msg)		npcHandler:onCreatureSay(cid, type, msg)		end
-function onThink()				npcHandler:onThink()					end
--- OTServ event handling functions end
+local vocation = {}
+local town = {}
+local destination = {}
 
+function onCreatureAppear(cid)              npcHandler:onCreatureAppear(cid)            end
+function onCreatureDisappear(cid)           npcHandler:onCreatureDisappear(cid)         end
+function onCreatureSay(cid, type, msg)      npcHandler:onCreatureSay(cid, type, msg)    end
+function onThink()                          npcHandler:onThink()                        end
 
-function oracle(cid, message, keywords, parameters, node)
-	if(cid ~= npcHandler.focus) then
+local function greetCallback(cid)
+	local player = Player(cid)
+	local level = player:getLevel()
+	if level < 8 then
+		npcHandler:say("CHILD! COME BACK WHEN YOU HAVE GROWN UP!", cid)
+		return false
+	elseif level > 9 then
+		npcHandler:say(player:getName() .. ", I CAN'T LET YOU LEAVE - YOU ARE TOO STRONG ALREADY! YOU CAN ONLY LEAVE WITH LEVEL 9 OR LOWER.", cid)
+		return false
+	elseif player:getVocation():getId() > 0 then
+		npcHandler:say("YOU ALREADY HAVE A VOCATION!", cid)
 		return false
 	end
-	
-	local cityNode = node:getParent():getParent()
-	local vocNode = node:getParent()
-	
-	local destination = cityNode:getParameters().destination
-	local townid = cityNode:getParameters().townid
-	local voc = vocNode:getParameters().voc
-	
-	if(destination ~= nil and voc ~= nil and townid ~= nil) then
-		if(getPlayerLevel(cid) < parameters.level) then
-			npcHandler:say('You must first reach level ' .. parameters.level .. '!')
-		else
-			if voc == 1 then
-                                                     
-                                                     doPlayerSendTextMessage(cid,22,"You are now a Sorcerer.")
-                                                     end
-                                                     if voc == 2 then
-                                                     
-                                                     doPlayerSendTextMessage(cid,22,"You are now a Druid.")
-                                                     end
-                                                     if voc == 3 then
-                                                    
-                                                     doPlayerSendTextMessage(cid,22,"You are now a Paladin.")
-                                                     end
-                                                     if voc == 4 then
-                                                     
-                                                     doPlayerSendTextMessage(cid,22,"You are now a Knight.")
-                                                     end
-                                                     doPlayerSetVocation(cid,voc)
-			doPlayerSetTown(cid,townid)
-        	                                   doTeleportThing(cid,destination)
-        	                                   doSendMagicEffect(destination,12)
-
-        end
-	else
-		error('Destination:', destination, 'Vocation:', vocation, 'Townid:', townid)
-	end
-	npcHandler:resetNpc()
 	return true
 end
 
-
-function greetCallback(cid)
-	if(getPlayerLevel(cid) < LEVEL) then
-		npcHandler:say('CHILD! COME BACK WHEN YOU HAVE GROWN UP!')
+local function creatureSayCallback(cid, type, msg)
+	if not npcHandler:isFocused(cid) then
 		return false
-	else
-		return true
 	end
+
+	if msgcontains(msg, "yes") and npcHandler.topic[cid] == 0 then
+		npcHandler:say("IN WHICH TOWN DO YOU WANT TO LIVE: {RHYVES}?", cid)
+		npcHandler.topic[cid] = 1
+	elseif npcHandler.topic[cid] == 1 then
+		if msgcontains(msg, "rhyves") then
+			town[cid] = 2
+			destination[cid] = Position(159, 387, 6)
+			npcHandler:say("IN RHYVES! AND WHAT PROFESSION HAVE YOU CHOSEN: {KNIGHT}, {PALADIN}, {SORCERER}, OR {DRUID}?", cid)
+			npcHandler.topic[cid] = 2
+		else
+			npcHandler:say("IN WHICH TOWN DO YOU WANT TO LIVE: {RHYVES}?", cid)
+		end
+	elseif npcHandler.topic[cid] == 2 then
+		if msgcontains(msg, "sorcerer") then
+			npcHandler:say("A SORCERER! ARE YOU SURE? THIS DECISION IS IRREVERSIBLE!", cid)
+			npcHandler.topic[cid] = 3
+			vocation[cid] = 1
+		elseif msgcontains(msg, "druid") then
+			npcHandler:say("A DRUID! ARE YOU SURE? THIS DECISION IS IRREVERSIBLE!", cid)
+			npcHandler.topic[cid] = 3
+			vocation[cid] = 2
+		elseif msgcontains(msg, "paladin") then
+			npcHandler:say("A PALADIN! ARE YOU SURE? THIS DECISION IS IRREVERSIBLE!", cid)
+			npcHandler.topic[cid] = 3
+			vocation[cid] = 3
+		elseif msgcontains(msg, "knight") then
+			npcHandler:say("A KNIGHT! ARE YOU SURE? THIS DECISION IS IRREVERSIBLE!", cid)
+			npcHandler.topic[cid] = 3
+			vocation[cid] = 4
+		else
+			npcHandler:say("{KNIGHT}, {PALADIN}, {SORCERER}, OR {DRUID}?", cid)
+		end
+	elseif npcHandler.topic[cid] == 3 then
+		if msgcontains(msg, "yes") then
+			local player = Player(cid)
+			npcHandler:say("SO BE IT!", cid)
+			player:setVocation(Vocation(vocation[cid]))
+			player:setTown(Town(town[cid]))
+
+			local destination = destination[cid]
+			npcHandler:releaseFocus(cid)
+			player:teleportTo(destination)
+			player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+			destination:sendMagicEffect(CONST_ME_TELEPORT)
+		else
+			npcHandler:say("THEN WHAT? {KNIGHT}, {PALADIN}, {SORCERER}, OR {DRUID}?", cid)
+			npcHandler.topic[cid] = 2
+		end
+	end
+	return true
 end
 
--- Set the greeting callback function
+local function onAddFocus(cid)
+	town[cid] = 0
+	vocation[cid] = 0
+	destination[cid] = 0
+end
+
+local function onReleaseFocus(cid)
+	town[cid] = nil
+	vocation[cid] = nil
+	destination[cid] = nil
+end
+
+npcHandler:setCallback(CALLBACK_ONADDFOCUS, onAddFocus)
+npcHandler:setCallback(CALLBACK_ONRELEASEFOCUS, onReleaseFocus)
+
 npcHandler:setCallback(CALLBACK_GREET, greetCallback)
-
--- Set the greeting message.
-npcHandler:setMessage(MESSAGE_GREET, 'Hello |PLAYERNAME|. Are you prepared to face your destiny?')
-
--- Pre-create the yes/no nodes.
-local yesNode = KeywordNode:new({'yes'}, oracle, {level = LEVEL})
-local noNode = KeywordNode:new({'no'}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, moveup = 1, text = 'Then what vocation do you want to become?'})
-
--- Create the actual keyword structure...
-local node1 = keywordHandler:addKeyword({'yes'}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, text = 'What city do you wish to live in? Carlin, Thais or Venore?'})
-	local node2 = node1:addChildKeyword({'carlin'}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, townid = 5, destination = {x=32360, y=31782, z=7}, text = 'Carlin will be your home-town! So what vocation do you wish to become? Sorcerer, druid, paladin or knight?'})
-		local node3 = node2:addChildKeyword({'sorcerer'}, StdModule.say, {npcHandler = npcHandler, voc = 1, onlyFocus = true, text = 'So, you wish to be a powerful magician? Are you sure about that? This decision is irreversible!'})
-			node3:addChildKeywordNode(yesNode)
-			node3:addChildKeywordNode(noNode)
-		local node3 = node2:addChildKeyword({'druid'}, StdModule.say, {npcHandler = npcHandler, voc = 2, onlyFocus = true, text = 'A druid! Are you sure? This decision is irreversible!'})
-			node3:addChildKeywordNode(yesNode)
-			node3:addChildKeywordNode(noNode)
-		local node3 = node2:addChildKeyword({'paladin'}, StdModule.say, {npcHandler = npcHandler, voc = 3, onlyFocus = true, text = 'A nimble paladin! Are you sure? This decision is irreversible!'})
-			node3:addChildKeywordNode(yesNode)
-			node3:addChildKeywordNode(noNode)
-		local node3 = node2:addChildKeyword({'knight'}, StdModule.say, {npcHandler = npcHandler, voc = 4, onlyFocus = true, text = 'A valorous knight! Are you sure? This decision is irreversible!'})
-			node3:addChildKeywordNode(yesNode)
-			node3:addChildKeywordNode(noNode)
-
-		local node2 = node1:addChildKeyword({'thais'}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, townid = 3, destination = {x=32369, y=32241, z=7}, text = 'Thais will be your home-town! So what vocation do you wish to become? Sorcerer, druid, paladin or knight?'})
-		local node3 = node2:addChildKeyword({'sorcerer'}, StdModule.say, {npcHandler = npcHandler, voc = 1, onlyFocus = true, text = 'So, you wish to be a powerful magician? Are you sure about that? This decision is irreversible!'})
-			node3:addChildKeywordNode(yesNode)
-			node3:addChildKeywordNode(noNode)
-		local node3 = node2:addChildKeyword({'druid'}, StdModule.say, {npcHandler = npcHandler, voc = 2, onlyFocus = true, text = 'A mighty druid! Are you sure? This decision is irreversible!'})
-			node3:addChildKeywordNode(yesNode)
-			node3:addChildKeywordNode(noNode)
-		local node3 = node2:addChildKeyword({'paladin'}, StdModule.say, {npcHandler = npcHandler, voc = 3, onlyFocus = true, text = 'A nimble paladin! Are you sure? This decision is irreversible!'})
-			node3:addChildKeywordNode(yesNode)
-			node3:addChildKeywordNode(noNode)
-		local node3 = node2:addChildKeyword({'knight'}, StdModule.say, {npcHandler = npcHandler, voc = 4, onlyFocus = true, text = 'A valorous knight! Are you sure? This decision is irreversible!'})
-			node3:addChildKeywordNode(yesNode)
-			node3:addChildKeywordNode(noNode)
-
-	local node2 = node1:addChildKeyword({'venore'}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, townid = 4, destination = {x=32957, y=32076, z=7}, text = 'Venore will be your home-town! So what vocation do you wish to become? Sorcerer, druid, paladin or knight?'})
-		local node3 = node2:addChildKeyword({'sorcerer'}, StdModule.say, {npcHandler = npcHandler, voc = 1, onlyFocus = true, text = 'So, you wish to be a powerful magician? Are you sure about that? This decision is irreversible!'})
-			node3:addChildKeywordNode(yesNode)
-			node3:addChildKeywordNode(noNode)
-		local node3 = node2:addChildKeyword({'druid'}, StdModule.say, {npcHandler = npcHandler, voc = 2, onlyFocus = true, text = 'A mighty druid! Are you sure? This decision is irreversible!'})
-			node3:addChildKeywordNode(yesNode)
-			node3:addChildKeywordNode(noNode)
-		local node3 = node2:addChildKeyword({'paladin'}, StdModule.say, {npcHandler = npcHandler, voc = 3, onlyFocus = true, text = 'A nimble paladin! Are you sure? This decision is irreversible!'})
-			node3:addChildKeywordNode(yesNode)
-			node3:addChildKeywordNode(noNode)
-		local node3 = node2:addChildKeyword({'knight'}, StdModule.say, {npcHandler = npcHandler, voc = 4, onlyFocus = true, text = 'A valorous knight! Are you sure? This decision is irreversible!'})
-			node3:addChildKeywordNode(yesNode)
-			node3:addChildKeywordNode(noNode)
-
-keywordHandler:addKeyword({'no'}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, text = 'Then come back when you are ready.'})
-
--- Make it react to hi/bye etc.
+npcHandler:setCallback(CALLBACK_MESSAGE_DEFAULT, creatureSayCallback)
 npcHandler:addModule(FocusModule:new())
--- spells/scripts/attack/energybomb.lua
+++ spells/scripts/attack/energybomb.lua
@@ -1,10 +1,19 @@
 local combat = createCombatObject()
-setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_ENERGYDAMAGE)
-setCombatParam(combat, COMBAT_PARAM_CREATEITEM, 1495)
+combat:setParameter(COMBAT_PARAM_TYPE, COMBAT_ENERGYDAMAGE)
+combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_ENERGYHIT)
+combat:setParameter(COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_ENERGY)
+combat:setParameter(COMBAT_PARAM_CREATEITEM, ITEM_ENERGYFIELD_PVP)
 
 local area = createCombatArea(AREA_SQUARE1X1)
-setCombatArea(combat, area)
+combat:setArea(area)
 
-function onCastSpell(cid, var)
-	return doCombat(cid, combat, var)
+function onCastSpell(creature, var)
+	-- check for stairHop delay
+	if not getCreatureCondition(creature, CONDITION_PACIFIED) then
+		return combat:execute(creature, var)
+	else
+		creature:sendCancelMessage(RETURNVALUE_YOUAREEXHAUSTED)
+		creature:getPosition():sendMagicEffect(CONST_ME_POFF)
+		return false
+	end
 end
